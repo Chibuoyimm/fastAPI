02244b4345642ac335af142bbb767d1d99743db0
Chibuoyim <onuigwechibuoyim@gmail.com>
2022-08-26 10:12:10 +0100

    Configured environment variables using .env file

--- app/config.py
+++ app/config.py
@@ -1,10 +1,18 @@
 from pydantic import BaseSettings
 
 
-class Settings(BaseSettings):
-    path: int
-    database_username: str = "postgres"
-    secret_key: str = "2478r3iwwfwhf27"
+class Settings(BaseSettings): # pydantic is not case sensitive
+    database_hostname: str # pydantic tries to typecast into specified datatype
+    database_port: str
+    database_password: str
+    database_name: str
+    database_username: str
+    secret_key: str
+    algorithm: str
+    access_token_expire_minutes: int
+
+    class Config:
+        env_file = r"C:\Users\onuig\Desktop\fastapi1\.env"
 
 
 settings = Settings()
